cc86d4000b8d4a78369d44e3ac074150c7bf56a3
suyeon woo <woosean999@gmail.com>
2023-04-24 14:43:01 +0900

    fix. name -> refName 으로 변경

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <div class="name">{{ name }}</div>
-  <button class="btn btn-primary" v-on:click="printGreeting('sean')">
+  <div class="name">{{ refName }}</div>
+  <button
+    class="btn btn-primary"
+    v-on:click="updateName(refName === 'su' ? 'sean' : 'su')"
+  >
     Click
   </button>
 </template>
 
 <script>
   // composition API
+  import {ref} from "vue";
+
   export default {
     setup() {
-      const name = "su";
+      let name = "su";
+      let refName = ref("su");   // ref 의 값은 문자열, 숫자, 배열, 오브젝트 타입 모두 가능
       const greeting = name => {
         return `Hello! ${name}`;
       };
 
-      function printGreeting(name) {
-        console.log(greeting(name));
+      function updateName(newName) {
+        name = newName; // 템플릿에 바로 반영되지는 않음
+        refName.value = newName; // 템플릿에 바로 반영됨( reactive )
+        console.log(name);
+        console.log(refName);
       }
 
       return {
         name,
         greeting,
-        printGreeting,
+        updateName,
+        refName,
       };
     },
   };
@@ -29,6 +39,6 @@
 
 <style scoped>
   .name {
-    color: aquamarine;
+    color: rgb(84, 141, 255);
   }
 </style>
