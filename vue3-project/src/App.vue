04d8109b9a530e8a3a9ecb87fa4dd94f8aaccd86
suyeon woo <woosean999@gmail.com>
2023-04-24 14:14:58 +0900

    add. 버튼생성, click 이벤트 추가

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="name">{{ name }}</div>
-  <div>함수 시그니처: {{ greeting }}</div>
-  <div>함수 아웃풋: {{ greeting(name) }}</div>
+  <button class="btn btn-primary" v-on:click="printGreeting('sean')">
+    Click
+  </button>
 </template>
 
 <script>
   // composition API
   export default {
     setup() {
-      const name = "suu";
+      const name = "su";
       const greeting = name => {
         return `Hello! ${name}`;
       };
 
-      console.log(greeting(name));
+      function printGreeting(name) {
+        console.log(greeting(name));
+      }
 
       return {
         name,
         greeting,
+        printGreeting,
       };
     },
   };
