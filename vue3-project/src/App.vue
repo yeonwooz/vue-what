42194a61568b580b6f08e048770e08264f9250a3
suyeon woo <woosean999@gmail.com>
2023-04-24 13:03:22 +0900

    add. setup 함수 안에서 변수와 함수 정의하고 렌더하기

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="name">{{ name }}</div>
+  <div>함수 시그니처: {{ greeting }}</div>
+  <div>함수 아웃풋: {{ greeting(name) }}</div>
 </template>
 
 <script>
@@ -7,9 +9,15 @@
   export default {
     setup() {
       const name = "suu";
+      const greeting = name => {
+        return `Hello! ${name}`;
+      };
+
+      console.log(greeting(name));
 
       return {
         name,
+        greeting,
       };
     },
   };
