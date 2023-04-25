311686e160a89b2c1adf82be73aab4032ec37c63
suyeon woo <woosean999@gmail.com>
2023-04-26 00:00:15 +0900

    add. v-show 인풋 에러메시지 추가

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <div v-show="toggle">true</div>
-  <div v-show="!toggle">false</div>
-  <button class="btn btn-secondary" @click="onToggle">TOGGLE</button>
   <div class="container">
-    <h2>TO-DO LIST</h2>
+    <h2>할일</h2>
     <form @submit.prevent="onSubmit" class="d-flex">
       <div class="flex-grow-1">
         <input
           class="form-control"
           type="text"
           v-model="todo"
-          placeholder="Type new to-do"
+          placeholder="무엇을 할까요?"
         />
+        <div style="color: red; font-size: 0.9rem" v-show="hasError">
+          할일을 입력해주세요
+        </div>
       </div>
       <div class="mx-2">
-        <button class="btn btn-primary" type="submit">ADD</button>
+        <button class="btn btn-primary" type="submit">추가</button>
       </div>
     </form>
 
@@ -29,31 +29,31 @@
 <script>
   /*
   // composition API:
-  Composition API is a set of APIs that allows us to author Vue components using imported functions instead of declaring options. 
+  Composition API is a set of APIs that allows us to author Vue components using imported functions instead of declaring options.
   */
   import {ref} from "vue";
 
   export default {
     setup() {
-      const toggle = ref(false);
       const todo = ref("");
       const todos = ref([]);
+      const hasError = ref(false);
 
       const onSubmit = () => {
-        todos.value.push({
-          id: Date.now(),
-          subject: todo.value,
-        });
-        todo.value = "";
-      };
-
-      const onToggle = () => {
-        toggle.value = !toggle.value;
+        if (todo.value) {
+          todos.value.push({
+            id: Date.now(),
+            subject: todo.value,
+          });
+          todo.value = "";
+          hasError.value = false;
+        } else {
+          hasError.value = true;
+        }
       };
 
       return {
-        toggle,
-        onToggle,
+        hasError,
         todo,
         todos,
         onSubmit,
