6734c385d48e0aaea97a3641f865ef6cd9173772
suyeon woo <woosean999@gmail.com>
2023-04-26 14:02:00 +0900

    add. 삭제기능 추가

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -17,10 +17,10 @@
         <button class="btn btn-primary" type="submit">추가</button>
       </div>
     </form>
-
+    <div class="p-2" v-if="!todos.length">아직 할일이 없습니다</div>
     <div class="card my-2" v-for="todo in todos" :key="todo.id">
-      <div class="card-body p-2">
-        <div class="form-check">
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
           <input
             class="form-check-input"
             type="checkbox"
@@ -29,6 +29,11 @@
           <label class="form-check-label" :class="{todo: todo.completed}">{{
             todo.subject
           }}</label>
+        </div>
+        <div>
+          <button class="btn btn-danger" @click="deleteTodo(todo.id)">
+            삭제
+          </button>
         </div>
       </div>
     </div>
@@ -66,12 +71,19 @@
         }
       };
 
+      const deleteTodo = id => {
+        console.log("before", todos);
+        todos.value = todos.value.filter(item => item.id !== id);
+        console.log("after", todos);
+      };
+
       return {
         hasError,
         todo,
         todos,
         onSubmit,
         todoStyle,
+        deleteTodo,
       };
     },
   };
