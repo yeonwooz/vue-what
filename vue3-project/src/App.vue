8dbfdf84b3c3cac9c813fef72f50909a2b68c98b
suyeon woo <woosean999@gmail.com>
2023-05-04 22:27:40 +0900

    add. 내비게이션 바 추가

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,3 +1,12 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand px-2" href="/"> SEAN's TODO </a>
+
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/todos">Todos</a>
+      </li>
+    </ul>
+  </nav>
   <router-view />
 </template>
