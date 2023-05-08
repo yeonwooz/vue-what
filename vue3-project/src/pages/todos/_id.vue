30f949777eb74db923566747b146664c05c00c34
suyeon woo <woosean999@gmail.com>
2023-05-08 17:20:40 +0900

    add. todoUpdated 추가

--- vue3-project/src/pages/todos/_id.vue
+++ vue3-project/src/pages/todos/_id.vue
@@ -25,32 +25,48 @@
         </div>
       </div>
     </div>
-    <button type="submit" class="btn btn-primary">저장</button>
-    <button type="button" class="btn btn-outline-dark mx-2" @click="moveToList">
-      취소
-    </button>
+    <div class="mt-2">
+      <button type="submit" class="btn btn-primary" :disabled="!todoUpdated">
+        저장
+      </button>
+      <button
+        type="button"
+        class="btn btn-outline-dark mx-2"
+        @click="moveToList"
+      >
+        취소
+      </button>
+    </div>
   </form>
 </template>
 
 <script>
   import axios from "axios";
   import {useRoute, useRouter} from "vue-router";
-  import {ref} from "@vue/reactivity";
+  import {ref, computed} from "vue";
+  import _ from "lodash";
+
   export default {
     setup() {
       const route = useRoute();
       const router = useRouter();
       const todo = ref(null);
+      const originalTodo = ref(null);
       const loading = ref(true);
       const SERVER_URL = "http://localhost:3000/todos";
       const todoId = route.params.id;
 
       const getTodo = async () => {
         const res = await axios.get(`${SERVER_URL}/${todoId}`);
-        todo.value = res.data;
+        originalTodo.value = {...res.data};
+        todo.value = {...res.data};
         loading.value = false;
       };
       getTodo();
+
+      const todoUpdated = computed(() => {
+        return !_.isEqual(todo.value, originalTodo.value);
+      });
 
       const toggleStatus = () => {
         todo.value.completed = !todo.value.completed;
@@ -63,12 +79,14 @@
       };
 
       const onSave = async () => {
+        if (!todoUpdated.value) return;
+        console.log("change");
         await axios.put(`${SERVER_URL}/${todoId}`, {
           subject: todo.value.subject,
           completed: todo.value.completed,
         });
       };
-      return {todo, toggleStatus, moveToList, onSave, loading};
+      return {todo, toggleStatus, moveToList, onSave, loading, todoUpdated};
     },
   };
 </script>
