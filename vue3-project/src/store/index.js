d710fa7475425756594d19dbed26b0ce8c853c85
suyeon woo <woosean999@gmail.com>
2023-05-09 15:54:18 +0900

    fix. store 관심사 분리

--- vue3-project/src/store/index.js
+++ vue3-project/src/store/index.js
@@ -1,49 +1,6 @@
 import {createStore} from "vuex";
+import modules from "./modules";
 
 export default createStore({
-  modules: {
-    toast: {
-      namespaced: true,
-      state: {
-        toastMessage: "",
-        toastType: "",
-        showToast: false,
-        timeout: null,
-      },
-
-      mutations: {
-        UPDATE_TOAST_MESSAGE(state, payload) {
-          state.toastMessage = payload;
-        },
-        UPDATE_TOAST_TYPE(state, payload) {
-          state.toastType = payload;
-        },
-        UPDATE_TOAST_STATUS(state, payload) {
-          state.showToast = payload;
-        },
-        UPDATE_TOAST_TIMEOUT(state, payload) {
-          state.timeout = payload;
-        },
-      },
-
-      actions: {
-        triggerToast({commit}, msg, type = "success") {
-          commit("UPDATE_TOAST_MESSAGE", msg);
-          commit("UPDATE_TOAST_TYPE", type);
-          commit("UPDATE_TOAST_STATUS", true);
-
-          setTimeout(() => {
-            commit("UPDATE_TOAST_MESSAGE", "");
-            commit("UPDATE_TOAST_TYPE", "");
-            commit("UPDATE_TOAST_STATUS", false);
-          }, 3000);
-        },
-      },
-      getters: {
-        toastMessageWithSmile(state) {
-          return state.toastMessage + " :)";
-        },
-      },
-    },
-  },
+  modules,
 });
